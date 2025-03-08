0eb45c5d435b8e725832bc295fb7f62843d8e3e6
rama <rraj95155@gmail.com>
2025-03-08 13:08:05 +0530

    Jenkins and yaml files

---
+++ Jenkins-Pipeline-Files/tf-azure-jenkinfile1.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    environment {
+        AZURE_CREDENTIALS = credentials('AZURE_CONNECTION')
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                        url: 'https://github.com/Ramaraju-devops/test-repos-demo.git',
+                        git branch: 'master',
+                        credentialsId: 'Github-Auth'
+                }
+            }
+
+        }
+        stage('Terraform INIT') {
+                steps {
+                    script {
+                            sh '''az login --service-principal -u $AZURE_CREDENTIALS_CLIENT_ID -p $AZURE_CREDENTIALS_CLIENT_SECRET --tenant $AZURE_CREDENTIALS_TENANT_ID
+                                az account show
+                                cd RG-Creation
+                                terraform init
+                                '''
+                    }
+            }
+        }
+        stage('Terraform Validate') {
+                steps {
+                    script {
+                            sh '''
+                                cd RG-Creation
+                                terraform validate
+                                '''
+                    }
+            }
+        }
+        stage('Terraform Plan') {
+                steps {
+                    script {
+                            sh '''
+                                cd RG-Creation
+                                terraform plan
+                                '''
+                    }
+            }
+        }
+    }
+
+
+}
